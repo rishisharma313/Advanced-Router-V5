883d9e8e013e74c311d4604eb1a36fa9fa762eb3
rishisharma313 <rishisharma313@gmail.com>
2022-01-06 11:45:48 +0530

    Create QuoteList.js

---
+++ src/components/quotes/QuoteList.js
@@ -0,0 +1,34 @@
+import { Fragment } from 'react';
+import { useHistory, useLocation} from 'react-router-dom';
+
+import QuoteItem from './QuoteItem';
+import classes from './QuoteList.module.css';
+
+const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+  const queryParamas = new URLSearchParams(location.search);
+  console.log(queryParamas.get('sort'));
+  const changeSortingHandler = () =>{
+    history.push('/quotes?sort=asc')
+  }
+  return (
+    <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort Ascending</button>
+      </div>
+      <ul className={classes.list}>
+        {props.quotes.map((quote) => (
+          <QuoteItem
+            key={quote.id}
+            id={quote.id}
+            author={quote.author}
+            text={quote.text}
+          />
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
+
+export default QuoteList;
